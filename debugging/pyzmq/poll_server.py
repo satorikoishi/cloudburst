e48c8c6d5695fcb077e1f633f3567f1fad58d27c
Wei Jin <410453494@qq.com>
2023-02-19 22:15:55 +0800

    poll debug

---
+++ debugging/pyzmq/poll_server.py
@@ -0,0 +1,29 @@
+#
+#   Hello World server in Python
+#   Binds REP socket to tcp://*:5555
+#   Expects b"Hello" from client, replies with b"World"
+#
+
+import time
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+poller = zmq.Poller()
+poller.register(socket, zmq.POLLIN)
+
+while True:
+    #  Wait for next request from client
+    socks = dict(poller.poll(timeout=1000))
+    
+    if socket in socks and socks[socket] == zmq.POLLIN:
+        msg = socket.recv_string()
+        message = socket.recv()
+        print("Received request: %s" % message)
+
+        #  Do some 'work'
+        time.sleep(1)
+
+        #  Send reply back to client
+        socket.send(b"World")
