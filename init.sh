93305f779b3d90e1bc4cb74b1c1486161b404113
Wei Jin <410453494@qq.com>
2023-02-19 16:36:07 +0800

    minor

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ sudo apt install -y protobuf-compiler python3-pip
 cd ~
 git clone https://github.com/satorikoishi/cloudburst.git
 cd cloudburst
+pip3 install --upgrade pip
 pip3 install -r requirements.txt
 git submodule update --init --recursive
 ./common/scripts/install-dependencies.sh
