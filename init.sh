0821eaec88ac2815b7fdaaf4f12e19e79adb99d6
Wei Jin <410453494@qq.com>
2023-02-20 22:59:39 +0800

    plus anna init

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 sudo apt update
 sudo apt install -y protobuf-compiler python3-pip
 cd ~
+
+# cloudburst
 git clone https://github.com/satorikoishi/cloudburst.git
 cd cloudburst
 pip3 install -r requirements.txt
@@ -8,3 +10,10 @@ git submodule update --init --recursive
 ./common/scripts/install-dependencies.sh
 sudo ./scripts/install-anna.sh
 ./scripts/build.sh
+cd ..
+
+# anna
+git clone --recurse-submodules https://github.com/satorikoishi/anna.git
+cd anna
+./scripts/build.sh -g
+cd ..
