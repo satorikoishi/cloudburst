95cf3eb38aea0185cf0473d42c0657873f3b0806
Wei Jin <410453494@qq.com>
2023-02-28 21:22:42 +0800

    k8s init

---
+++ k8s.sh
@@ -0,0 +1,33 @@
+sudo apt-get update
+
+# docker
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# config
+sudo swapoff -a
+echo '[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc] \
+  [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options] \
+    SystemdCgroup = true' > config.toml
+sudo mv config.toml /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+# k8s
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
