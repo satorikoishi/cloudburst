b575e1612c0ea514c0f2d6e8a12a5cfa061a19b5
Wei Jin <410453494@qq.com>
2023-03-09 16:07:16 +0800

    refine

--- cloudburst/debugging/connection/anna_client.py
+++ cloudburst/debugging/connection/anna_client.py
@@ -5,7 +5,8 @@ from cloudburst.shared.serializer import Serializer
 serializer = Serializer()
 
 if len(sys.argv) < 3:
-    print('Usage: ./cloudburst_client.py ROUTE_SERVICE_ADDR MY_IP')
+    print('Usage: python3 ./cloudburst_client.py ROUTE_SERVICE_ADDR MY_IP')
+    exit()
 
 local = False
 elb_addr = sys.argv[1]
@@ -14,7 +15,15 @@ client_ip = sys.argv[2]
 kvs_client = AnnaTcpClient(elb_addr, client_ip, local=local,
                                         offset=10)
 
-lattice = serializer.dump_lattice('2')
-print("Writing to kvs")
-kvs_client.put('1', lattice)
-print("Write finished")
+key = '1'
+value = '2'
+
+lattice = serializer.dump_lattice(value)
+print("Writing to kvs, key = 1, value = 2")
+res = kvs_client.put(key, lattice)
+print(f"Write finished with {res}")
+
+print("Reading from kvs, key = 1")
+get_lattice = kvs_client.get(key)[key]
+get_v = serializer.load_lattice(get_lattice)
+print(f"Read get value {get_v}")
--- cloudburst/debugging/connection/cloudburst_client.py
+++ cloudburst/debugging/connection/cloudburst_client.py
@@ -2,7 +2,8 @@ import sys
 from cloudburst.client.client import CloudburstConnection
 
 if len(sys.argv) < 3:
-    print('Usage: ./cloudburst_client.py FUNC_SERVICE_ADDR MY_IP')
+    print('Usage: python3 ./cloudburst_client.py FUNC_SERVICE_ADDR MY_IP')
+    exit()
 
 local = False
 elb_addr = sys.argv[1]
@@ -12,4 +13,7 @@ print("Establishing connection to scheduler...")
 conn = CloudburstConnection(elb_addr, client_ip, local=local)
 
 cloud_sq = conn.register(lambda _, x: x * x, 'square')
-print(cloud_sq(2).get())
+print("Register success, try call function")
+
+res = cloud_sq(2).get()
+print(f"Function res: {res}")
