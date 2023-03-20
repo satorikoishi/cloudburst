69ca1948ba290909907c414636df06fd26921806
Wei Jin <410453494@qq.com>
2023-03-20 20:10:37 +0800

    fix k_hop args

--- cloudburst/server/benchmarks/comparison/k_hop.py
+++ cloudburst/server/benchmarks/comparison/k_hop.py
@@ -40,7 +40,7 @@ def create_dag(cloudburst_client):
             return sum
         
         for friend_id in friends:
-            sum += k_hop(friend_id, k - 1)
+            sum += k_hop(cloudburst, friend_id, k - 1)
         
         return sum
 
