07d1390471e47bf051e8cf7113782b520f6cbba1
Wei Jin <410453494@qq.com>
2023-03-27 20:07:08 +0800

    debug

---
+++ cloudburst/debugging/shredder/client.py
@@ -0,0 +1,19 @@
+import sys
+from cloudburst.shared.kvs_client import ShredderKvsClient
+
+if len(sys.argv) < 3:
+    print('Usage: python3 ./client.py Shredder_ip, ')
+    exit()
+    
+client = ShredderKvsClient(host=sys.argv[1], port=6379, db=0)
+key = sys.argv[2]
+
+if len(sys.argv) == 4:
+    print(f'Client put key {key}, value of len {sys.argv[3]}')
+    value = 'x' * sys.argv[3]
+    res = client.put(key, value)
+else:
+    print(f'Client get key {key}')
+    res = client.get(key)
+
+print(res)
