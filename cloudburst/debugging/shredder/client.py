f3641f654ce5bee40cddcf21878841724d8d9898
Wei Jin <410453494@qq.com>
2023-03-27 21:18:25 +0800

    lattice

--- cloudburst/debugging/shredder/client.py
+++ cloudburst/debugging/shredder/client.py
@@ -1,5 +1,8 @@
 import sys
 from cloudburst.shared.kvs_client import ShredderKvsClient
+from cloudburst.shared.serializer import Serializer
+
+serializer = Serializer()
 
 if len(sys.argv) < 3:
     print('Usage: python3 ./client.py Shredder_ip, ')
@@ -12,9 +15,12 @@ if len(sys.argv) == 4:
     repeat = int(sys.argv[3])
     print(f'Client put key {key}, value of len {repeat}')
     value = 'x' * repeat
-    res = client.put(key, value)
+    
+    lattice = serializer.dump_lattice(value)
+    res = client.put(key, lattice)
 else:
     print(f'Client get key {key}')
-    res = client.get(key)
+    get_lattice = client.get(key)[key]
+    res = serializer.load_lattice(get_lattice)
 
 print(res)
