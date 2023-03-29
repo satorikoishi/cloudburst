b32983833a67a40a67bf7c78149785f845ba2a61
Yuan-Allen <hangyu.yuan@qq.com>
2023-03-29 17:09:19 +0800

    fix: smaller `ARR_SIZE`

--- cloudburst/server/benchmarks/micro/meta.py
+++ cloudburst/server/benchmarks/micro/meta.py
@@ -1,14 +1,14 @@
-SIZE_UPPER_BOUND = 4    # For quick debugging
+# SIZE_UPPER_BOUND = 4    # For quick debugging
 # SIZE_UPPER_BOUND = 7
 
-ARR_SIZE = []
+ARR_SIZE = [1, 8, 32, 128, 512]
 
-for i in range(1, SIZE_UPPER_BOUND):
-    # Value size from 80B to 8MB
-    ARR_SIZE.append(pow(10, i)) 
+# for i in range(1, SIZE_UPPER_BOUND):
+#     # Value size from 80B to 8MB
+#     ARR_SIZE.append(pow(10, i)) 
 
 NUM_KV_PAIRS = 1000
 
 def key_gen(size, idx):
-    # To compatible with lattice
+    # To be compatible with lattice
     return str(NUM_KV_PAIRS * size + idx)
